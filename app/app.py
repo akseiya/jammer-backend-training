b862df6c92870ffe1dbf013340f3605914770807
Michał Lech <akseiya@gmail.com>
2020-03-02 06:07:11 -0800

    gunicorn logging enabled and hooked into Flask

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import logging
 from eve import Eve
 app = Eve()
 
@@ -5,9 +6,16 @@ app = Eve()
 def tadek():
     return 'Tadek dość chętnie i często ssie penisy.'
 
-# This is not actually run really, since we want Gunicorn to reload
-# changing dev code, while eve/flask itself is not supposed to be
-# used for deployed code.
-
 if __name__ == '__main__':
+    # This can only happen on a dev machine with no containers used
     app.run(host="0.0.0.0")
+else:
+    # Hook up logging to gunicorn, using gunicorn's logging
+    # level specified outside the app
+    # (https://medium.com/@trstringer/logging-flask-and-gunicorn-the-manageable-way-2e6f0b8beb2f)
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.logger.info(
+        open('docs/appstart.txt', 'r').read()
+    )
