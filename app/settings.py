84960a40effe6838985270f6d9e6bd76a4fbfb26
Michał Lech <akseiya@gmail.com>
2020-03-02 12:06:07 -0800

    Eve schema is now a YAML file

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,7 @@
-MONGO_HOST="mongo"
-MONGO_PORT=27017
-MONGO_DBNAME="eve"
-DOMAIN = {'people': {}}
+import yaml
+
+MONGO_HOST      = "mongo"
+MONGO_PORT      =  27017
+MONGO_DBNAME    = "eve"
+with open('app/eve_schema.yml') as f:
+    DOMAIN = yaml.safe_load(f)
